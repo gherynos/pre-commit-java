f12734e676f89d36bbe2ee5173ce3aa5c655bf3e
Gherynos <gherynos@naes.co>
2023-04-23 10:15:00 +0100

    Updated automation

--- tag.sh
+++ tag.sh
@@ -3,15 +3,15 @@
 set -e
 
 if ! git diff-index --quiet HEAD; then
-  git config --global user.name 'Gherynos'
-  git config --global user.email 'gherynos@users.noreply.github.com'
+  git config --global user.name 'github-actions[bot]'
+  git config --global user.email '41898282+github-actions[bot]@users.noreply.github.com'
   git commit -am "patch: update tools versions"
   git push
 
   version=$(git describe --abbrev=0 --tags)
   new_version=$(perl -pe 's/(\d+)(?!.*\d+)/$1+1/e' <<< "$version")
   echo "$version -> $new_version"
-  git tag "temp"
+  git tag "$new_version"
 
   git push --tags
 fi
