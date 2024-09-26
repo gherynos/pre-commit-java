0c1159c1a332ec68f3c6ddbc766792706568a0e4
Christian Decker <chrisdecker1201@users.noreply.github.com>
2024-09-26 16:16:50 +0200

    pmd: The `format` parameter can now be overwritten.

--- run_pmd.sh
+++ run_pmd.sh
@@ -16,10 +16,15 @@ if [[ ! $pc_args == *"--use-version="* ]]; then
   pc_args="$pc_args --use-version=java-17"
 fi
 
+# add default format if not specified
+if [[ ! $pc_args == *"-f="* && ! $pc_args == *"--format="* ]]; then
+  pc_args="$pc_args -f=textcolor"
+fi
+
 # add default ruleset if not specified
 if [[ ! $pc_args == *"-R="* && ! $pc_args == *"--rulesets="* ]]; then
   pc_args="$pc_args -R=/opt/ruleset.xml"
 fi
 
 # shellcheck disable=SC2086
-/opt/pmd/bin/pmd check --no-progress -f=textcolor --file-list=/tmp/list $pc_args
+/opt/pmd/bin/pmd check --no-progress --file-list=/tmp/list $pc_args
